1fae9f556115a806849d2c22017f5928d92cf8a5
friend1ws <friend1ws@gmail.com>
2020-05-26 06:02:49 +0000

    modifications

--- nanobreak.py
+++ nanobreak.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python
 
 import sys, os, gzip, subprocess, csv, shutil, statistics, tempfile
-import pysam
+import pysam, edlib
 import nanomonsv
+
+csv.field_size_limit(sys.maxsize)
 
 def reverse_complement(seq):
     complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A',
@@ -139,7 +141,10 @@ def cluster_breakpoints(tumor_bp_file, normal_bp_file, output_file, min_tumor_re
 
             # skip secondary alignment
             if row[11] == "True": continue
-        
+            
+            # skip decoy
+            if "decoy" in row[0]: continue
+    
             if row[0] != cluster_flusher.chr:
                 cluster_flusher.flush_lines('+')
                 cluster_flusher.flush_lines('-')
@@ -386,153 +391,174 @@ def locate_breakpoint(input_file, output_file, reference_file, margin = 200):
 
 
 
-def long_read_validate(input_file, tumor_bam, normal_bam, output_file, reference, validate_sequence_length = 200, score_ratio_thres = 1.4, start_pos_thres = 0.2, end_pos_thres = 0.8,
-    min_tumor_variant_read_num = 3, min_tumor_VAF = 0.05, max_control_variant_read_num = 1, max_control_VAF = 0.03):
 
-    def count_read_by_alignment(input_file, bam_file, output_file, reference, validate_sequence_length = 200, score_ratio_thres = 1.4, start_pos_thres = 0.2, end_pos_thres = 0.8):
+def count_read_by_alignment(input_file, bam_file, output_file, reference, validate_sequence_length = 200, score_ratio_thres = 1.4, start_pos_thres = 0.2, end_pos_thres = 0.8):
 
-        class Alignment_counter(object):
+    class Alignment_counter(object):
 
-            def __init__(self, score_ratio_thres, start_pos_thres, end_pos_thres):
-                self.key = ''
-                # self.hout = open(output_file, 'w')
-                self.query_seq = None
-                self.target_seq_list = []
-                self.score_ratio_thres = score_ratio_thres
-                self.start_pos_thres = start_pos_thres
-                self.end_pos_thres = end_pos_thres
-                self.tmp_dir = tempfile.mkdtemp()
-                
- 
-            def __del__(self):
-                # self.hout.close()
-                shutil.rmtree(self.tmp_dir)
+        def __init__(self, score_ratio_thres, start_pos_thres, end_pos_thres):
+            self.key = ''
+            # self.hout = open(output_file, 'w')
+            self.query_seq = None
+            self.target_seq_list = []
+            self.score_ratio_thres = score_ratio_thres
+            self.start_pos_thres = start_pos_thres
+            self.end_pos_thres = end_pos_thres
+            self.tmp_dir = tempfile.mkdtemp()
+            
 
-            def initialize(self, key, query_seq):
-                self.key = key
-                self.query_seq = query_seq
-                self.target_seq_list = []
+        def __del__(self):
+            # self.hout.close()
+            shutil.rmtree(self.tmp_dir)
 
-            def add_query_seq(self, read_name, read_seq):
-                self.target_seq_list.append((read_name, read_seq))
+        def initialize(self, key, query_seq):
+            self.key = key
+            self.query_seq = query_seq
+            self.target_seq_list = []
+
+        def add_query_seq(self, read_name, read_seq):
+            self.target_seq_list.append((read_name, read_seq))
+
+        def count_alignment(self):
+
+            if len(self.target_seq_list) == 0: return None
+            cluster_id, _, _, _ = key.split('\t')
+
+            """
+            with open(self.tmp_dir + '/' + cluster_id + ".target.fa", 'w') as hout_ta:
+                for read in self.target_seq_list:
+                    print(">%s\n%s" % (read[0], read[1]), file = hout_ta)
+
+            with open(self.tmp_dir + '/' + cluster_id + ".query.fa", 'w') as hout_qu:
+                print(">query_%s\n%s" % (cluster_id, self.query_seq), file = hout_qu)
+
+            alignment_info = nanomonsv.long_read_validate.ssw_check(
+                self.tmp_dir + '/' + cluster_id + ".query.fa",
+                self.tmp_dir + '/' + cluster_id + ".target.fa")
+
+            all_rnames = list(set(alignment_info.keys()))
+            supporting_reads = [rname for rname in all_rnames if \
+                alignment_info[rname][0] > self.score_ratio_thres * len(self.query_seq) and \
+                alignment_info[rname][1] < self.start_pos_thres * len(self.query_seq) and \
+                alignment_info[rname][2] > self.end_pos_thres * len(self.query_seq)]
+
+            if "15033196" in self.key:
+                for a in alignment_info: print(a, alignment_info[a], a in supporting_reads)
+                print(' ')
+            """
+            print(cluster_id)
+
+            all_rnames = []
+            supporting_reads = []
+            align_res = []
+            for target_read in self.target_seq_list:
+                all_rnames.append(target_read[0])                                                          
+                tres = edlib.align(self.query_seq, target_read[1], mode = "HW", task = "locations")
+                print(target_read[0])
+                print(tres) 
+                if tres["editDistance"] < len(self.query_seq) * 0.25: # and \
+                    # tres["locations"][0][0] < 0.2 * len(self.query_seq) and \
+                    # tres["locations"][0][1] > 0.8 * len(self.query_seq):
+                        supporting_reads.append(target_read[0])
+
+            return (len(all_rnames), len(supporting_reads))
+
     
-            def count_alignment(self):
-
-                if len(self.target_seq_list) == 0: return None
-                cluster_id, _, _, _ = key.split('\t')
-
-                with open(self.tmp_dir + '/' + cluster_id + ".target.fa", 'w') as hout_ta:
-                    for read in self.target_seq_list:
-                        print(">%s\n%s" % (read[0], read[1]), file = hout_ta)
-
-                with open(self.tmp_dir + '/' + cluster_id + ".query.fa", 'w') as hout_qu:
-                    print(">query_%s\n%s" % (cluster_id, self.query_seq), file = hout_qu)
-
-                alignment_info = nanomonsv.long_read_validate.ssw_check(
-                    self.tmp_dir + '/' + cluster_id + ".query.fa",
-                    self.tmp_dir + '/' + cluster_id + ".target.fa")
-
-                all_rnames = list(set(alignment_info.keys()))
-                supporting_reads = [rname for rname in all_rnames if \
-                    alignment_info[rname][0] > self.score_ratio_thres * len(self.query_seq) and \
-                    alignment_info[rname][1] < self.start_pos_thres * len(self.query_seq) and \
-                    alignment_info[rname][2] > self.end_pos_thres * len(self.query_seq)]
-
-                if "15033196" in self.key:
-                    for a in alignment_info: print(a, alignment_info[a], a in supporting_reads)
-                    print(' ')
-
-                return (len(all_rnames), len(supporting_reads))
-
-        
-        bam_hin = pysam.AlignmentFile(bam_file, 'rb')
-        reference_fasta = pysam.FastaFile(reference)
-        rname2key = {}
-        key2contig = {}
-        with open(input_file, 'r') as hin:
-            for row in csv.reader(hin, delimiter = '\t'):
-                key = '\t'.join(row[:4])
-                for read in bam_hin.fetch(row[1], max(int(row[2]) - 100, 0), int(row[2]) + 100):
-                    if read.is_secondary: continue
-                    if read.qname not in rname2key: rname2key[read.qname] = []
-                    rname2key[read.qname].append(key)
-    
-                if row[3] == '+':
-                    contig = reference_fasta.fetch(row[1], max(int(row[2]) - validate_sequence_length - 1, 0), int(row[2]))
-                else:
-                    contig = reference_fasta.fetch(row[1], int(row[2]) - 1, int(row[2]) + validate_sequence_length - 1)
-                    contig = reverse_complement(contig)
-                contig = contig + row[4][:validate_sequence_length]
-                key2contig[key] = contig
-                    
-
-        for rname in rname2key:
-            keys = list(set(rname2key[rname]))
-            rname2key[rname] = keys
-
-        with open(output_file + ".tmp.long_read_seq.unsorted", 'w') as hout:
-            for read in bam_hin.fetch():
-
-                if read.is_secondary or read.is_supplementary: continue
-                if read.qname in rname2key:
-                    read_seq = read.query_sequence
-                    read_seq = reverse_complement(read.query_sequence) if read.is_reverse else read.query_sequence
-                    for key in rname2key[read.qname]:
-                        print("%s\t%s\t%s" % (key, read.qname, read_seq), file = hout)
-
-        bam_hin.close()
-
-        with open(output_file + ".tmp.long_read_seq.sorted", 'w') as hout:
-            subprocess.check_call(["sort", "-k1,1", output_file + ".tmp.long_read_seq.unsorted"], stdout = hout)
-        os.remove(output_file + ".tmp.long_read_seq.unsorted")
-    
-        key2count = {}
-        alignment_counter = Alignment_counter(score_ratio_thres, start_pos_thres, end_pos_thres)
-        with open(output_file + ".tmp.long_read_seq.sorted", 'r') as hin:
-            for row in csv.reader(hin, delimiter = '\t'):
-                key = '\t'.join(row[:4])
-                if key != alignment_counter.key:
-                    acount = alignment_counter.count_alignment()
-                    if acount is not None:
-                        key2count[alignment_counter.key] = acount
-
-                    alignment_counter.initialize(key, key2contig[key])
-                alignment_counter.add_query_seq(row[4], row[5])
-
-            acount = alignment_counter.count_alignment()
-            if acount is not None:
-                key2count[alignment_counter.key] = acount
-
-        del alignment_counter
-        os.remove(output_file + ".tmp.long_read_seq.sorted")
-
-
-        return key2count
-
-
-    key2count_tumor = count_read_by_alignment(input_file, tumor_bam, output_file, reference, validate_sequence_length = 200, score_ratio_thres = 1.4, start_pos_thres = 0.2, end_pos_thres = 0.8)
-    key2count_normal = count_read_by_alignment(input_file, normal_bam, output_file, reference, validate_sequence_length = 200, score_ratio_thres = 1.4, start_pos_thres = 0.2, end_pos_thres = 0.8)
-
+    bam_hin = pysam.AlignmentFile(bam_file, 'rb')
+    reference_fasta = pysam.FastaFile(reference)
+    rname2key = {}
     key2contig = {}
     with open(input_file, 'r') as hin:
         for row in csv.reader(hin, delimiter = '\t'):
             key = '\t'.join(row[:4])
-            key2contig[key] = row[4]
+            for read in bam_hin.fetch(row[1], max(int(row[2]) - 100, 0), int(row[2]) + 100):
+                if read.is_secondary: continue
+                if read.qname not in rname2key: rname2key[read.qname] = []
+                rname2key[read.qname].append(key)
 
-    with open(output_file, 'w') as hout:
-        for key in key2count_tumor:
-            count_tumor = key2count_tumor[key]
-            count_normal = key2count_normal[key] if key in key2count_normal else (0, 0)
+            if row[3] == '+':
+                contig = reference_fasta.fetch(row[1], max(int(row[2]) - validate_sequence_length - 1, 0), int(row[2]))
+            else:
+                contig = reference_fasta.fetch(row[1], int(row[2]) - 1, int(row[2]) + validate_sequence_length - 1)
+                contig = reverse_complement(contig)
+            contig = contig + row[4][:validate_sequence_length]
+            key2contig[key] = contig
+                
 
-            if count_tumor[0] == 0: continue
-            if count_tumor[1] < min_tumor_variant_read_num: continue
-            if float(count_tumor[1]) / float(count_tumor[0]) < min_tumor_VAF: continue
+    for rname in rname2key:
+        keys = list(set(rname2key[rname]))
+        rname2key[rname] = keys
 
-            if count_normal[0] == 0: continue
-            if count_normal[1] > max_control_variant_read_num: continue
-            if float(count_normal[1]) / float(count_normal[0]) > max_control_VAF: continue
+    with open(output_file + ".tmp.long_read_seq.unsorted", 'w') as hout:
+        for read in bam_hin.fetch():
 
-            print("%s\t%d\t%d\t%d\t%d\t%s" % (key, count_tumor[0], count_tumor[1], count_normal[0], count_normal[1], key2contig[key]), file = hout)
+            if read.is_secondary or read.is_supplementary: continue
+            if read.qname in rname2key:
+                read_seq = read.query_sequence
+                read_seq = reverse_complement(read.query_sequence) if read.is_reverse else read.query_sequence
+                for key in rname2key[read.qname]:
+                    print("%s\t%s\t%s" % (key, read.qname, read_seq), file = hout)
+
+    bam_hin.close()
+
+    with open(output_file + ".tmp.long_read_seq.sorted", 'w') as hout:
+        subprocess.check_call(["sort", "-k1,1", output_file + ".tmp.long_read_seq.unsorted"], stdout = hout)
+    os.remove(output_file + ".tmp.long_read_seq.unsorted")
+
+    # key2count = {}
+    alignment_counter = Alignment_counter(score_ratio_thres, start_pos_thres, end_pos_thres)
+    with open(output_file + ".tmp.long_read_seq.sorted", 'r') as hin, open(output_file, 'w') as hout:
+        for row in csv.reader(hin, delimiter = '\t'):
+            key = '\t'.join(row[:4])
+            if key != alignment_counter.key:
+                acount = alignment_counter.count_alignment()
+                if acount is not None:
+                    print("%s\t%d\t%d" % (alignment_counter.key, acount[0], acount[1]), file = hout)
+                    # key2count[alignment_counter.key] = acount
+
+                alignment_counter.initialize(key, key2contig[key])
+            alignment_counter.add_query_seq(row[4], row[5])
+
+        acount = alignment_counter.count_alignment()
+        if acount is not None:
+            print("%s\t%d\t%d" % (alignment_counter.key, acount[0], acount[1]), file = hout)
+            # key2count[alignment_counter.key] = acount
+
+    del alignment_counter
+    os.remove(output_file + ".tmp.long_read_seq.sorted")
+    # return key2count
+
+
+def join_count_result(consensus_file, tumor_count_file, normal_count_file, output_file, min_tumor_variant_read_num = 3, min_tumor_VAF = 0.05, max_control_variant_read_num = 1, max_control_VAF = 0.03):
+
+    key2tumor_count = {}
+    with open(tumor_count_file, 'r') as hin:
+        for row in csv.reader(hin, delimiter = '\t'):
+            key = '\t'.join(row[:4])
+            key2tumor_count[key] = (int(row[4]), int(row[5]))
+
+    key2normal_count = {}
+    with open(normal_count_file, 'r') as hin:
+        for row in csv.reader(hin, delimiter = '\t'):
+            key = '\t'.join(row[:4])
+            key2normal_count[key] = (int(row[4]), int(row[5]))
+
+    with open(consensus_file, 'r') as hin, open(output_file, 'w') as hout:
+        for row in csv.reader(hin, delimiter = '\t'):
+            key = '\t'.join(row[:4])
+            consensus = row[4]
+            if key not in key2tumor_count or key not in key2normal_count: continue
+            tumor_count = key2tumor_count[key]
+            normal_count = key2normal_count[key]
+
+            if tumor_count[0] == 0: continue
+            if normal_count[0] == 0: continue
+            if tumor_count[1] < min_tumor_variant_read_num: continue
+            if normal_count[1] > max_control_variant_read_num: continue
+            if float(tumor_count[1]) / tumor_count[0] < min_tumor_VAF: continue
+            if float(normal_count[1]) / normal_count[0] > max_control_VAF: continue
+
+            print("%s\t%d\t%d\t%d\t%d\t%s" % (key, tumor_count[0], tumor_count[1], normal_count[0], normal_count[1], consensus), file = hout)
 
 
  
@@ -545,6 +571,7 @@ if __name__ == "__main__":
     output_prefix = sys.argv[3]
     reference_file = sys.argv[4]
 
+    """
     ##########
     # tumor parse
     parse_alignment_info(tumor_bam, output_prefix + ".tmp.tumor.bp_parse.bed")
@@ -580,7 +607,6 @@ if __name__ == "__main__":
     os.remove(output_prefix + ".tmp.normal.bp_parse.bed")
     os.remove(output_prefix + ".tmp.normal.bp_parse.sorted.bed")
     ##########
-
     cluster_breakpoints(output_prefix + ".tmp.tumor.bp_parse.sorted.bed.gz", 
                         output_prefix + ".tmp.normal.bp_parse.sorted.bed.gz",
                         output_prefix + ".tmp.nbp_parse.cluster_info.txt")
@@ -593,5 +619,21 @@ if __name__ == "__main__":
     locate_breakpoint(output_prefix + ".tmp.nbp_parse.cluster_info_consensus.txt", 
         output_prefix + ".tmp.nbp_parse.bp_info_consensus.txt",
         reference_file)
+    """
 
-    long_read_validate(output_prefix + ".tmp.nbp_parse.bp_info_consensus.txt", tumor_bam, normal_bam, output_prefix + ".tmp.bp_cluster.alignment_count.txt", reference_file)
+    count_read_by_alignment(output_prefix + ".tmp.nbp_parse.bp_info_consensus.txt", 
+        tumor_bam, 
+        output_prefix + ".tmp.tumor.bp_cluster.alignment_count.txt",
+        reference_file)
+
+    count_read_by_alignment(output_prefix + ".tmp.nbp_parse.bp_info_consensus.txt", 
+        normal_bam, 
+        output_prefix + ".tmp.normal.bp_cluster.alignment_count.txt",
+        reference_file) 
+
+    join_count_result(output_prefix + ".tmp.nbp_parse.bp_info_consensus.txt",
+                      output_prefix + ".tmp.tumor.bp_cluster.alignment_count.txt",
+                      output_prefix + ".tmp.normal.bp_cluster.alignment_count.txt",
+                      output_prefix + ".nanobreak.result.txt")
+
+
